adcb78af93d08588324b0222b4b42b8e590ca376
Fuad <fuadarizik@gmail.com>
2022-09-14 15:42:05 +0200

    Removed Ecks demo script

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const mineflayerViewer = require('prismarine-viewer').mineflayer
 const inventoryViewer = require('mineflayer-web-inventory')
 
 const bot = mineflayer.createBot({
-    host: 'creative.ultravanilla.world', // minecraft server ip
-    username: 'Fuad_R', //* minecraft username
+    host: 'creative.ultravanilla.world', //! minecraft server ip
+    username: 'Fuad_R', //! minecraft username
     //* password: '', // minecraft password, comment out if you want to log into online-mode=false servers
-    //*  port: 25565,                // only set if you need a port that isn't 25565
-    version: "1.18.2",           //* only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+    //*  port: 25565,               // only set if you need a port that isn't 25565
+    version: "1.18.2",              //* only set if you need a specific version or snapshot
     auth: 'microsoft',              //* only set if you need microsoft auth, then set this to 'microsoft
     keepAlive: 'false'
   })
@@ -45,9 +45,9 @@ let options = {
   //* webPath: PATH,
   //* express: EXPRESS,
   //* app: APP,
-  http: HTTP,
+  //* http: HTTP,
   //* io: IO,
-  startOnLoad: True,
+  //* startOnLoad: True,
   //* windowUpdateDebounceTime: INT
 }
 
